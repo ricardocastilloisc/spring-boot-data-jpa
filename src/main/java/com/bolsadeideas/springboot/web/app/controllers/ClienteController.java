af52d9ae22fccb5cdd0e477f0c0a7a7860808cf8
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-07-03 13:04:08 -0500

    056 Agregando reglas de validacion usando anotaciones

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/ClienteController.java
@@ -2,9 +2,12 @@ package com.bolsadeideas.springboot.web.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,7 +36,13 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) 
+		{
+			model.addAttribute("titulo","Formulario de Cliente");
+			return "form";
+		}
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
--- src/main/java/com/bolsadeideas/springboot/web/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/entity/Cliente.java
@@ -12,6 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,10 +26,19 @@ public class Cliente implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotEmpty
+	@Size(min=4, max=12)
 	private String nombre;
+	
+	@NotEmpty
 	private String apellido;
+	
+	@NotEmpty
+	@Email
 	private String email;
 	
+	@NotNull
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
