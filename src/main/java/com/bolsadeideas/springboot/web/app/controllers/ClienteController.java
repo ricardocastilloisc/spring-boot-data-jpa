d46edc53db06a444dbd45df1c66bb66b0a1ae7df
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-07-03 13:57:24 -0500

    060 Agregando eliminar

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/ClienteController.java
@@ -65,4 +65,12 @@ public class ClienteController {
 		return "form";
 	}
 	
+	@RequestMapping(value = "/eliminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id) {
+		if(id > 0) {
+			clienteDao.delete(id);
+		}
+		return "redirect:/listar";
+	}
+	
 }
--- src/main/java/com/bolsadeideas/springboot/web/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/dao/ClienteDaoImpl.java
@@ -36,8 +36,15 @@ public class ClienteDaoImpl implements IClienteDao{
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Cliente findOne(Long id) {
 		return em.find(Cliente.class, id);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		em.remove(findOne(id));
 	}
 
 }
