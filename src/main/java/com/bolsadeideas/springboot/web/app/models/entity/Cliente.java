fd1e348599a0254f3f9ebfe5a0abb70eff5570e9
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-07-03 12:18:36 -0500

    053 Creando la vista crear y formulario

--- src/main/java/com/bolsadeideas/springboot/web/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,9 +23,15 @@ public class Cliente implements Serializable{
 	private String nombre;
 	private String apellido;
 	private String email;
+	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
 	
 	public Long getId() {
 		return id;
