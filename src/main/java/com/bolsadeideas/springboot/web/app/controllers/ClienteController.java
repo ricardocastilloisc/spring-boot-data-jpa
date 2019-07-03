21df5947274e4bd5846e75d8e9ad59b6395fa102
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-07-03 13:45:42 -0500

    059 Agregando editar

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/ClienteController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.web.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.web.app.models.entity.Cliente;
@@ -36,7 +38,7 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		
 		if(result.hasErrors()) 
 		{
@@ -44,7 +46,23 @@ public class ClienteController {
 			return "form";
 		}
 		clienteDao.save(cliente);
-		return "redirect:listar";
+		status.setComplete();
+		return "redirect:/listar";
+	}
+	
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+		Cliente cliente = null;
+		if(id > 0) {
+			cliente = clienteDao.findOne(id);
+		}else {
+			return "redirect:/listar";
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
+		
+		return "form";
 	}
 	
 }
