9b17ab0c802acfa2795b414923f426a308863b67
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-07-09 14:51:04 -0500

    076 Configuraciones y preparando la aplicacion

--- src/main/java/com/bolsadeideas/springboot/web/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/entity/Cliente.java
@@ -44,6 +44,18 @@ public class Cliente implements Serializable{
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	
+	private String foto;
+	
+	
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
 	public Long getId() {
 		return id;
 	}
